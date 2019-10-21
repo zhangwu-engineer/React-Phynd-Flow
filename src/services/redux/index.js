9feca95e416d4f60e3da0ce5106490ed2924805a
Zhang Wu <zhangwu.engineer@gmail.com>
2019-10-21 05:05:33 -0400

    update store configuration

--- src/services/redux/index.js
+++ src/services/redux/index.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore as reduxCreateStore, compose } from 'redux'
 import { createBrowserHistory } from 'history'
 import createSagaMiddleware from 'redux-saga'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, createMigrate } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import sagas from 'sagas/index'
 import createRootReducer from 'reducers/index'
@@ -13,10 +13,21 @@ const rootReducer = createRootReducer(history)
 const sagaMiddleware = createSagaMiddleware()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+//ensure that changes to persisted store are migrated from one version to the next
+const migrations = {
+  0: state => {
+    return {
+      ...state,
+    }
+  }
+}
+
 export const store = reduxCreateStore(
   persistReducer({
     key: 'root',
     storage,
+    version: 0,
+    migrate: createMigrate(migrations, { debug: true }),
   }, rootReducer),
   initialState,
   composeEnhancers(
