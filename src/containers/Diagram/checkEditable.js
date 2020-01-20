b2f6e14d61e4f841269e22bb13007b9e0916ec16
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-20 15:14:05 -0500

    separate checkEditable modules

---
+++ src/containers/Diagram/checkEditable.js
@@ -0,0 +1,32 @@
+export const checkCategoryEditable = (node) => {
+    if ((node.edges && node.edges.length === 0) || node.parent) {
+      if (
+        node.data.parentType === 'conditional-entity' ||
+        node.data.parentType === 'switch-entity' ||
+        node.data.parentType === 'regex-info' ||
+        node.data.parentType === 'function-info' ||
+        node.data.parentType === 'iteration-info' ||
+        node.data.parentType === 'jsonproperty-info' ||
+        node.data.parentType === 'aggregate-info' ||
+        node.data.parentType === 'aggregate-iterator-info' ||
+        node.data.parentType === 'jsonelement-info' ||
+        node.data.parentType === 'jsonelement-operations'
+      )
+        return false;
+      return true;
+    }
+    return false;
+  }
+  
+  export const checkNodeEditable = node => (
+      node.data.parentType === 'Constant' ||
+      node.data.parentType === 'Column' ||
+      node.data.parentType === 'HL7' ||
+      node.data.parentType === 'regex-info' ||
+      node.data.parentType === 'function-info' ||
+      node.data.parentType === 'iteration-info' ||
+      node.data.parentType === 'jsonproperty-info' ||
+      node.data.parentType === 'aggregate-info' ||
+      node.data.parentType === 'aggregate-iterator-info' ||
+      node.data.parentType === 'jsonelement-info'
+    ) ? true : false;
--- src/containers/Diagram/index.js
+++ src/containers/Diagram/index.js
@@ -7,6 +7,7 @@ import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 
 import { generateInitialSource } from './generateInitialSource';
+import { checkNodeEditable, checkCategoryEditable } from './checkEditable';
 
 const DIAGRAM_CONF = {
   DIAGRAM_PADDING: 30,
@@ -1122,39 +1123,6 @@ const getChildrenWeight = (field) => {
   }
   return 1;
 }
-
-const checkCategoryEditable = (node) => {
-  if ((node.edges && node.edges.length === 0) || node.parent) {
-    if (
-      node.data.parentType === 'conditional-entity' ||
-      node.data.parentType === 'switch-entity' ||
-      node.data.parentType === 'regex-info' ||
-      node.data.parentType === 'function-info' ||
-      node.data.parentType === 'iteration-info' ||
-      node.data.parentType === 'jsonproperty-info' ||
-      node.data.parentType === 'aggregate-info' ||
-      node.data.parentType === 'aggregate-iterator-info' ||
-      node.data.parentType === 'jsonelement-info' ||
-      node.data.parentType === 'jsonelement-operations'
-    )
-      return false;
-    return true;
-  }
-  return false;
-}
-
-const checkNodeEditable = node => (
-    node.data.parentType === 'Constant' ||
-    node.data.parentType === 'Column' ||
-    node.data.parentType === 'HL7' ||
-    node.data.parentType === 'regex-info' ||
-    node.data.parentType === 'function-info' ||
-    node.data.parentType === 'iteration-info' ||
-    node.data.parentType === 'jsonproperty-info' ||
-    node.data.parentType === 'aggregate-info' ||
-    node.data.parentType === 'aggregate-iterator-info' ||
-    node.data.parentType === 'jsonelement-info'
-  ) ? true : false;
 
 const Diagram = forwardRef(({ source, elementId, triggerModal, triggerDetailsModal, triggerCaseKeyModal, updateDashboard, triggerOperationModal }, ref) => {
   const [elements, setElements] = React.useState([]);
