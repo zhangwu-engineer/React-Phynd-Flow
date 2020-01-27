6d83a45d81a175a2b735d5ae1c583a72c156911e
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-27 16:12:45 -0500

    smoke test - App renders without crashing

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -13,9 +13,7 @@ describe('To test the App Component functionality.', () => {
   let rootComponent = null;
 
   it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    shallow(<App />);
   });
 
   it(`should mount and check if Provider component exists.`, () => {
