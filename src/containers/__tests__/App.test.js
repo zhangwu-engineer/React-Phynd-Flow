0ec1f980fe603767f821442cca9d773cfe49ea85
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-27 13:25:54 -0500

    check if it has got a redux provider

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { App } from '../App';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('To test the App Component functionality.', () => {
+  configure({ adapter: new Adapter() });
+  let rootComponent = null;
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it(`should mount and check if it has got a provider.`, () => {
+    rootComponent = mount(<App />);
+      const isReduxProvider = rootComponent.find(Provider);
+      expect(isReduxProvider).toHaveLength(1);
+  });
 });
