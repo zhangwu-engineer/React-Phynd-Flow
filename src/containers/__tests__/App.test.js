8bf5532c4e1de10efdd0a7cc8788671aa8478333
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-27 15:47:01 -0500

    check if Provider and PersistGate exist

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { configure, mount } from 'enzyme';
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 import { App } from '../App';
 
 describe('To test the App Component functionality.', () => {
@@ -15,9 +18,16 @@ describe('To test the App Component functionality.', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it(`should mount and check if it has got a provider.`, () => {
+  it(`should mount and check if Provider component exists.`, () => {
     rootComponent = mount(<App />);
-      const isReduxProvider = rootComponent.find(Provider);
-      expect(isReduxProvider).toHaveLength(1);
+    const isReduxProvider = rootComponent.find(Provider);
+    expect(isReduxProvider).toHaveLength(1);
   });
+
+  it(`should mount and check if PersistGate component exis`, () => {
+    rootComponent = mount(<App />);
+    const isPersistGate = rootComponent.find(PersistGate);
+    expect(isPersistGate).toHaveLength(1);
+  });
+
 });
