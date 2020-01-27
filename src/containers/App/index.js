8033a768611457b64dc086ea9c674adc759e5798
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-27 02:15:04 -0500

    separate App Component

---
+++ src/containers/App/index.js
@@ -0,0 +1,30 @@
+import 'babel-polyfill';
+import 'services/immutability'
+import React from 'react';
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider } from 'styled-components'
+import { store, persistor, history } from 'services/redux'
+import Routes from 'Routes'
+import { muiTheme, styledTheme } from 'services/bootstrap';
+
+const AppContainer = () => (
+  <MuiThemeProvider theme={muiTheme}>
+    <ThemeProvider theme={styledTheme}>
+      <HashRouter basename='/' history={history}>
+        <Routes history={history} />
+      </HashRouter>
+    </ThemeProvider>
+  </MuiThemeProvider>
+)
+
+export const App = () => (
+  <Provider store={store}>
+    <PersistGate loading="null" persistor={persistor}>
+      <AppContainer />
+    </PersistGate>
+  </Provider>
+)
+
--- src/index.js
+++ src/index.js
@@ -2,32 +2,11 @@ import 'babel-polyfill';
 import 'services/immutability'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
-import { PersistGate } from 'redux-persist/integration/react'
-import { MuiThemeProvider } from '@material-ui/core/styles'
-import { ThemeProvider } from 'styled-components'
-import { store, persistor, history } from 'services/redux'
-import Routes from 'Routes'
-import { muiTheme, styledTheme } from 'services/bootstrap';
+import { App } from 'containers/App';
 import * as serviceWorker from './serviceWorker';
 
-const AppContainer = () => (
-  <MuiThemeProvider theme={muiTheme}>
-    <ThemeProvider theme={styledTheme}>
-      <HashRouter basename='/' history={history}>
-        <Routes history={history} />
-      </HashRouter>
-    </ThemeProvider>
-  </MuiThemeProvider>
-)
-
 ReactDOM.render((
-  <Provider store={store}>
-    <PersistGate loading="null" persistor={persistor}>
-      <AppContainer />
-    </PersistGate>
-  </Provider>
+  <App />
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
