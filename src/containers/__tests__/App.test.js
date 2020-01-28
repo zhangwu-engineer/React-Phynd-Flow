cea945624e4c728e8b58e472baf51485851a42fe
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-28 02:53:36 -0500

    PersisteGate component

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 
@@ -22,6 +21,12 @@ describe('To test the App Component functionality.', () => {
     rootComponent = mount(<App />);
     const isReduxProvider = rootComponent.find(Provider);
     expect(isReduxProvider).toHaveLength(1);
+  });
+
+  it(`should mount and check if PersistGate component exists`, () => {
+    rootComponent = mount(<App />);
+    const isPersistGate = rootComponent.find(PersistGate);
+    expect(isPersistGate).toHaveLength(1);
   });
 
   it(`should mount and check if AppContainer component exists`, () => {
