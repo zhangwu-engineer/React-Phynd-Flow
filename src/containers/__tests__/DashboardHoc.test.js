783046b4e51a7c996d2f638b0e0f6955e4e900f9
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-29 03:56:28 -0500

    separate hoc testing

--- src/containers/__tests__/DashboardHoc.test.js
+++ src/containers/__tests__/DashboardHoc.test.js
@@ -22,15 +22,18 @@ describe('<Hoc />', () => {
   it('should render the component only when prop is not null', () => {
     const DashboardHOC = hoc(<Dashboard />);
     const wrapper = shallow(<DashboardHOC store={store} />);
-
     expect(wrapper).not.toBe(null);
+  });
+
+  it('should render the hoc', () => {
+    const DashboardHOC = hoc(<Dashboard />);
+    const wrapper = shallow(<DashboardHOC store={store} />);
+    expect(wrapper.find('withRouter(ProvidersHoc)')).toHaveLength(1);
   });
 
   it('should generate props correctly', () => {
     const DashboardHOC = hoc(<Dashboard />);
     const wrapper = shallow(<DashboardHOC store={store} />);
-    
-    expect(wrapper.find('withRouter(ProvidersHoc)')).toHaveLength(1);
     expect(wrapper.find('withRouter(ProvidersHoc)').props().dashboardReducer).toBe(undefined);
     expect(wrapper.find('withRouter(ProvidersHoc)').props().sidebarData).toStrictEqual({});
     expect(wrapper.find('withRouter(ProvidersHoc)').props().isContactMap).toStrictEqual(false);
