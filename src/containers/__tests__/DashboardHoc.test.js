b4abb7f4292009ca14a2c32dd5f8dd960547b7e9
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-28 22:13:44 -0500

    wrap Dashboard component with empty dashboardReducer

--- src/containers/__tests__/DashboardHoc.test.js
+++ src/containers/__tests__/DashboardHoc.test.js
@@ -15,15 +15,12 @@ describe('<Hoc />', () => {
 
     // creates the store with any initial state or middleware needed
     store = mockStore({
-      user: {
-        isLoaded: true,
-      },
+      dashboardReducer: {},
     });
   });
-  it('should render the component only when prop is true', () => {
-    const Component = <Dashboard />;
-    const ConditionalHOC = hoc(Component);
-    const wrapper = shallow(<ConditionalHOC store={store} />);
+  it('should render the component only when prop is not null', () => {
+    const DashboardHOC = hoc(<Dashboard />);
+    const wrapper = shallow(<DashboardHOC store={store} />);
     expect(wrapper).not.toBe(null);
   });
 });
