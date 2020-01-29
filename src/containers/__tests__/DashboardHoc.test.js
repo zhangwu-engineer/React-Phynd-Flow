6d01cbd7d793c485749b62272f6f1ab79fa4de1b
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-29 04:39:03 -0500

    beforeEach defines shallow DashboardHOC

--- src/containers/__tests__/DashboardHoc.test.js
+++ src/containers/__tests__/DashboardHoc.test.js
@@ -6,7 +6,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import hoc from '../Dashboard/hoc';
 import Dashboard from '../Dashboard';
 
-let store;
+let store, DashboardHOC, wrapper;
 configure({ adapter: new Adapter() });
 
 describe('<Hoc />', () => {
@@ -17,27 +17,27 @@ describe('<Hoc />', () => {
     store = mockStore({
       isLoaded: true,
     });
+
+    DashboardHOC = hoc(<Dashboard />);
+    wrapper = shallow(<DashboardHOC store={store} />);
   });
 
   it('should render the component only when prop is not null', () => {
-    const DashboardHOC = hoc(<Dashboard />);
-    const wrapper = shallow(<DashboardHOC store={store} />);
     expect(wrapper).not.toBe(null);
   });
 
-  it('should render the hoc', () => {
-    const DashboardHOC = hoc(<Dashboard />);
-    const wrapper = shallow(<DashboardHOC store={store} />);
+  it('should render the hoc with router', () => {
     expect(wrapper.find('withRouter(ProvidersHoc)')).toHaveLength(1);
   });
 
-  it('should generate props correctly', () => {
-    const DashboardHOC = hoc(<Dashboard />);
-    const wrapper = shallow(<DashboardHOC store={store} />);
+  it('should generate default props correctly', () => {
     expect(wrapper.find('withRouter(ProvidersHoc)').props().dashboardReducer).toBe(undefined);
+    expect(wrapper.find('withRouter(ProvidersHoc)').props().dashboardList).not.toBe(null);
+    expect(wrapper.find('withRouter(ProvidersHoc)').props().filedsReducer).toBe(undefined);
+    expect(wrapper.find('withRouter(ProvidersHoc)').props().fieldsList).toBe(undefined);
+    expect(wrapper.find('withRouter(ProvidersHoc)').props().blockList).toBe(undefined);
     expect(wrapper.find('withRouter(ProvidersHoc)').props().sidebarData).toStrictEqual({});
     expect(wrapper.find('withRouter(ProvidersHoc)').props().isContactMap).toStrictEqual(false);
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().dashboardList).not.toBe(null);
   });
 
 });
