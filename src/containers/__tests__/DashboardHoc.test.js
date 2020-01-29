55e5825d95df043511b065bbfdc665e489e682e1
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-29 11:23:32 -0500

    separate testing for default wrapper

--- src/containers/__tests__/DashboardHoc.test.js
+++ src/containers/__tests__/DashboardHoc.test.js
@@ -6,10 +6,18 @@ import Adapter from 'enzyme-adapter-react-16';
 import hoc from '../Dashboard/hoc';
 import Dashboard from '../Dashboard';
 
-let store, DashboardHOC, wrapper;
-configure({ adapter: new Adapter() });
+let store, DashboardHOCDefault, wrapperDefault, DashboardHOC, wrapper;
+const mockFn = jest.fn();
+const dashboardProps = {
+  getDashboardDataRequest: new mockFn(),
+  updateDashboardDataRequest: new mockFn(),
+  updateFieldsDataRequest: new mockFn(),
+  getFieldsPerEntityRequest: new mockFn(),
+};
 
 describe('<Hoc />', () => {
+  configure({ adapter: new Adapter() });
+
   beforeEach(() => {
     const mockStore = configureStore();
 
@@ -18,26 +26,31 @@ describe('<Hoc />', () => {
       isLoaded: true,
     });
 
-    DashboardHOC = hoc(<Dashboard />);
+    DashboardHOCDefault = hoc(<Dashboard />);
+    wrapperDefault = shallow(<DashboardHOCDefault store={store} />);
+
+    DashboardHOC = hoc(<Dashboard {...dashboardProps} />);
     wrapper = shallow(<DashboardHOC store={store} />);
   });
 
   it('should render the component only when prop is not null', () => {
+    expect(wrapperDefault).not.toBe(null);
     expect(wrapper).not.toBe(null);
   });
 
   it('should render the hoc with router', () => {
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)')).toHaveLength(1);
     expect(wrapper.find('withRouter(ProvidersHoc)')).toHaveLength(1);
   });
 
   it('should generate default props correctly', () => {
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().dashboardReducer).toBe(undefined);
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().dashboardList).not.toBe(null);
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().filedsReducer).toBe(undefined);
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().fieldsList).toBe(undefined);
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().blockList).toBe(undefined);
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().sidebarData).toStrictEqual({});
-    expect(wrapper.find('withRouter(ProvidersHoc)').props().isContactMap).toStrictEqual(false);
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)').props().dashboardReducer).toBe(undefined);
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)').props().dashboardList).not.toBe(null);
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)').props().filedsReducer).toBe(undefined);
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)').props().fieldsList).toBe(undefined);
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)').props().blockList).toBe(undefined);
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)').props().sidebarData).toStrictEqual({});
+    expect(wrapperDefault.find('withRouter(ProvidersHoc)').props().isContactMap).toStrictEqual(false);
   });
 
 });
