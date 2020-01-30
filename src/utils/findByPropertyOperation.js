85f5eda43193f9255616cea979c6be1364cf144f
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-30 11:12:52 -0500

    findByPropertyOperation

---
+++ src/utils/findByPropertyOperation.js
@@ -0,0 +1,14 @@
+export const findByPropertyOperation = (obj, val,  name, field, value)=> {
+  for (let p in obj) {
+    if (p === 'Element' && `elementoperations-entity-${obj['MappingFieldId']}` === val) {
+      obj[p]['Operations'].push({
+        name,
+        field,
+        value,
+        Source: {},
+      });
+    } else if (typeof obj[p] === 'object') {
+      findByPropertyOperation(obj[p], val);
+    }
+  }
+};
