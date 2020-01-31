be52c7c0fe73472d5e616e7744eed8f546fab924
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-31 10:15:30 -0500

    WIP - findByDiagram

--- src/utils/findByDiagram.js
+++ src/utils/findByDiagram.js
@@ -1,13 +1,17 @@
 export const findByDiagram = (cyListener, parent) => {
-  const elements = cyListener && cyListener._private && cyListener._private.elements;
+  const elements = cyListener &&
+    cyListener._private &&
+    cyListener._private.elements;
   let foundIndex = 0;
   elements.some(function (element) {
     foundIndex++;
     return element._private.data.id === parent.data.id;
   });
+  
+  console.log(elements);
 
   while (foundIndex < elements.length) {
-    if (elements[foundIndex]._private.data.parentType === parent.data.nextType) {
+    if (elements[foundIndex]._private.data.id.indexOf(parent.data.id) > -1) {
       return elements[foundIndex]._private;
     }
     foundIndex++;
