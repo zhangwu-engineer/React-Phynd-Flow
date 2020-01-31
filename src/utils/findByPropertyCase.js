3d26e1162d817a49a448b15070519c9186b12251
Zhang Wu <zhangwu.engineer@gmail.com>
2020-01-31 14:33:02 -0500

    fix findByPropertyCase to handle next recurrence

--- src/utils/findByPropertyCase.js
+++ src/utils/findByPropertyCase.js
@@ -2,14 +2,14 @@ export const findByPropertyCase = (obj, val, inputKeyValue)=> {
   for (let p in obj) {
     if (Array.isArray(obj[p]) && p === 'Cases' && `case-target-${obj['MappingFieldId']}` === val) {
       obj[p].push({
-        Key: inputKeyValue.length > 0 ? inputKeyValue : 'N/A',
+        Key: (inputKeyValue && inputKeyValue.length > 0) ? inputKeyValue : 'N/A',
         Value: {
           MappingFieldId: null,
           MappingFieldType: null,
         },
       });
     } else if (typeof obj[p] === 'object') {
-      findByPropertyCase(obj[p], val);
+      findByPropertyCase(obj[p], val, inputKeyValue);
     }
   }
 };
