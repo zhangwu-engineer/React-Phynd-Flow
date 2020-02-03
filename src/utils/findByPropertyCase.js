ef4d205788ed08d5235712acfe26099037d2b241
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-03 04:59:37 -0500

    update findByPropertyCase

--- src/utils/findByPropertyCase.js
+++ src/utils/findByPropertyCase.js
@@ -3,7 +3,9 @@ import { generateInitialSource } from 'utils/helper';
 export const findByPropertyCase = (obj, val, inputKeyValue, element, inputValue)=> {
   const nextMappingSource = generateInitialSource(element, null, inputValue);
   for (let p in obj) {
-    if (Array.isArray(obj[p]) && p === 'Cases' && `case-target-${obj['MappingFieldId']}` === val) {
+    if (Array.isArray(obj[p]) && p === 'Cases' &&
+      `case-target-${obj['MappingFieldId']}` === val
+    ) {
       obj[p].push({
         Key: (inputKeyValue && inputKeyValue.length > 0) ? inputKeyValue : 'N/A',
         Value: nextMappingSource,
