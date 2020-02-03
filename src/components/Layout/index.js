4ee61fb498d942e79357e7fc4105a819de4050c5
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-03 16:18:13 -0500

    button group in top module bar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,21 +2,43 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { moduleTabNumber, moduleRouter, a11yProps } from 'utils/helper';
-
 const useStyles = makeStyles(theme => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
   },
   toolbar: theme.mixins.toolbar,
   moduleBar: {
-    boxShadow: 'none'
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    boxShadow: 'none',
+  },
+  buttonGroup: {
+    boxShadow: 'none',
   }
 }));
+
+const StyledButton = withStyles({
+  root: {
+    borderRadius: 0,
+    border: 0,
+    color: 'white',
+    height: 34,
+    padding: '0 30px',
+    marginRight: 24,
+  },
+  label: {
+    textTransform: 'capitalize',
+    fontSize: 14,
+  },
+})(Button);
 
 const LayoutContainer = ({ children, history }) => {
   const classes = useStyles();
@@ -50,6 +72,17 @@ const LayoutContainer = ({ children, history }) => {
             <Tab label="Network Module" {...a11yProps(2)} />
             <Tab label="HealthPlan Module" {...a11yProps(3)} />
           </Tabs>
+          <ButtonGroup
+            variant="contained"
+            size="small"
+            color="primary"
+            aria-label="contained primary button group"
+            className={classes.buttonGroup}
+          >
+            <StyledButton>Submit</StyledButton>
+            <StyledButton>Compare</StyledButton>
+            <StyledButton>Revert</StyledButton>
+          </ButtonGroup>
         </Paper>
       </AppBar>
       {children}
