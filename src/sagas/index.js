55e02622194f99322851bd57dd08bbf6d9958fff
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-05 03:17:24 -0500

    link stashesSaga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects'
 import dashboardSaga from './dashboard'
 import fieldsSaga from './fields'
+import stashesSaga from './stashes'
 
 export default function* () {
   yield all([
     dashboardSaga(),
     fieldsSaga(),
+    stashesSaga(),
   ])
 }
