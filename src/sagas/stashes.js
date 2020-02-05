a28b1871c9d827d9a91529f8f918a8ddc81b812f
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-05 02:32:25 -0500

    init stashes saga

---
+++ src/sagas/stashes.js
@@ -0,0 +1,34 @@
+import { call, put, takeEvery } from 'redux-saga/effects'
+import * as constants from 'constants/index'
+import * as actions from 'actions'
+
+import * as queries from 'api'
+
+function* getStashesDataRequest({ payload }) {
+  try {
+    const data = yield call(queries.getEntityDataRequest.bind(null, {
+      ...payload,
+    }))
+
+    yield put(actions.getStashesDataSuccess({ data, message: 'Stashes have been fetched' }))
+  } catch (e) {
+    console.error(e)
+    yield put(actions.getStashesDataFailure({ message: e.message }))
+  }
+}
+
+function* updateStashesDataRequest({ payload }) {
+  try {
+    const data = payload.data;
+    yield put(actions.updateStashesDataSuccess({ data, message: 'Stashes have been updated' }))
+  } catch (e) {
+    console.error(e)
+    yield put(actions.updateStashesDataFailure({ message: e.message }))
+  }
+}
+
+
+export default function* () {
+  yield takeEvery(constants.GET_STASHES_DATA_REQUEST, getStashesDataRequest);
+  yield takeEvery(constants.UPDATE_STASHES_DATA_REQUEST, updateStashesDataRequest);
+}
