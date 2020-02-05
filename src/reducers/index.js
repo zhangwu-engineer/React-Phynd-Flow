3562f3c2da0f90926845cb4ad66db090c913ccab
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-05 02:14:12 -0500

    import stashes reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 
 import dashboard from './dashboard'
 import fields from './fields'
+import stashes from './stashes'
 
 export default (history) => combineReducers({
   dashboard,
   fields,
+  stashes,
   router: connectRouter(history),
 })
