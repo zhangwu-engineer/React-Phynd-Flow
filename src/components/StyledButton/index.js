fd6b4abfe0c463c0ff515d0fc5f9e09cb731c32e
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-06 04:07:44 -0500

    generate new StyledButton

---
+++ src/components/StyledButton/index.js
@@ -0,0 +1,17 @@
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+export const StyledButton = withStyles({
+  root: {
+    borderRadius: 0,
+    border: 0,
+    color: 'white',
+    height: 34,
+    padding: '0 30px',
+    marginRight: 24,
+  },
+  label: {
+    textTransform: 'capitalize',
+    fontSize: 14,
+  },
+})(Button);
