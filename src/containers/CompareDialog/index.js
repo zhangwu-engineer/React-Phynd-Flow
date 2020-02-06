0c3fc764528d9c8f53c117e4cc6e28b8d0e0be5d
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-06 04:08:20 -0500

    display actions button group

--- src/containers/CompareDialog/index.js
+++ src/containers/CompareDialog/index.js
@@ -4,13 +4,15 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import Typography from '@material-ui/core/Typography';
+import { StyledButton } from 'components/StyledButton';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { Column, Table } from 'react-virtualized';
 import 'react-virtualized/styles.css';
 // style
 import useStyles from './style';
 
 const height = 400;
-const rowHeight = 40;
+const rowHeight = 50;
 const headerHeight = 30;
 const width = 800;
 
@@ -19,6 +21,19 @@ const CompareDialog = ({ isModalShown, stashesList, hideModal }) => {
   const closeModal = () => {
     hideModal();
   }
+
+  const cellRenderer= ({ cellData, rowIndex, dataKey }) => (
+    <ButtonGroup
+      variant="contained"
+      size="small"
+      color="primary"
+      aria-label="contained primary button group"
+      className={classes.buttonGroup}
+    >
+      <StyledButton>Submit</StyledButton>
+      <StyledButton>Revert</StyledButton>
+    </ButtonGroup>
+  )
 
   return (
     <Dialog
@@ -42,9 +57,16 @@ const CompareDialog = ({ isModalShown, stashesList, hideModal }) => {
             rowGetter={({index}) => stashesList[index]}
             className={classes.stashTable}
           >
-            <Column className={classes.stashColumn} label="Module" dataKey="module" width={200} />
-            <Column className={classes.stashColumn}  label="Entity" dataKey="entity" width={200} />
-            <Column className={classes.stashColumn}  label="Name" dataKey="itemName" width={200} />
+            <Column className={classes.stashColumn} label="Module" dataKey="module" width={180} />
+            <Column className={classes.stashColumn} label="Entity" dataKey="entity" width={180} />
+            <Column className={classes.stashColumn} label="Name" dataKey="itemName" width={180} />
+            <Column
+              className={classes.stashColumn}
+              label="Actions"
+              dataKey="itemName"
+              cellRenderer={cellRenderer}
+              width={260}
+            />
           </Table>
         </Grid>
       </DialogContent>
