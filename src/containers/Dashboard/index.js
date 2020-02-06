50ceee61cc943d548dc6424f154f38e2811b1b9b
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-06 03:51:11 -0500

    save module/entity as display name

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -136,8 +136,8 @@ const Dashboard = ({
       itemName,
       itemContent,
       panelIndex,
-      module: match.params.module,
-      entity: match.params.entity,
+      module: getNameFromID(match.params.module),
+      entity: getNameFromEntity(match.params.entity),
     });
   }
 
