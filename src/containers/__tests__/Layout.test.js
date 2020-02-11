113cbd1f39c08ee1795e669073e72f7d58ab89b8
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-11 15:20:42 -0500

    Layout generates 3 StyledButtons

--- src/containers/__tests__/Layout.test.js
+++ src/containers/__tests__/Layout.test.js
@@ -3,10 +3,17 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Layout from 'components/Layout';
+import StyledButton from 'components/StyledButton';
 
+let layoutComponent;
+const historyMock = { push: jest.fn() };
 
 describe('To test the Layout Component functionality.', () => {
   configure({ adapter: new Adapter() });
+
+  beforeEach(() => {
+    layoutComponent = shallow(<Layout history={historyMock} />);
+  });
 
   it("renders without crashing", () => {
     const layoutComponentDefault = shallow(<Layout />);
@@ -14,9 +21,11 @@ describe('To test the Layout Component functionality.', () => {
   });
 
   it("renders with history props", () => {
-    const historyMock = { push: jest.fn() };
-    const layoutComponent = shallow(<Layout history={historyMock} />);
     expect(layoutComponent).toMatchSnapshot();
+  });
+
+  it("generates 3 StyledButtons", () => {
+    expect(layoutComponent.find(StyledButton)).toHaveLength(3);
   });
 
 
