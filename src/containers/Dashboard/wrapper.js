7bf573742c1d4d3ee0435bf0f4ccf8200fa76da7
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-12 12:03:37 -0500

    construct WrapperContainer

--- src/containers/Dashboard/wrapper.js
+++ src/containers/Dashboard/wrapper.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import ReactNofitication from 'react-notifications-component';
 
-const WrapperContainer = ({}) => {
+import LayoutContainer from 'components/Layout';
+import Dashboard from 'containers/Dashboard';
+import CompareDialog from 'containers/CompareDialog';
+
+const WrapperContainer = ({
+  history,
+  dashboardReducer,
+  dashboardList,
+  fieldsList,
+  isContactMap,
+  stashesList,
+  updateDashboard,
+  stashData,
+  updateFields,
+  revertCTA,
+  submitCTA,
+  compareCTA,
+  submitOne,
+  revertOne,
+}) => {
+
+  const [isCompareModalShown, setIsCompareModalShown] = React.useState(false);
 
   return (
     <div>
-      
+      <ReactNofitication />
+      <LayoutContainer
+        history={history}
+        revertCTA={revertCTA}
+        submitCTA={submitCTA}
+        compareCTA={compareCTA}
+      />
+      <Dashboard
+        dashboardReducer={dashboardReducer}
+        dashboardList={dashboardList}
+        fieldsList={fieldsList}
+        isContactMap={isContactMap}
+        updateDashboard={updateDashboard}
+        stashData={stashData}
+        updateFields={updateFields}
+      />
+      <CompareDialog
+        isModalShown={isCompareModalShown}
+        stashesList={stashesList}
+        hideModal={() => setIsCompareModalShown(false)}
+        submitCTA={submitCTA}
+        submitOne={submitOne}
+        revertOne={revertOne}
+      />
     </div>
   )
 }
