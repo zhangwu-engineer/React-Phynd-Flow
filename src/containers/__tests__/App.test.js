0d12e021e406ed02a9135b0f37d8c9a1ec75c01f
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-12 02:53:28 -0500

    disable multi mount of App

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -15,7 +15,11 @@ import { AppContainer } from '../App';
 
 describe('To test the App Component functionality.', () => {
   configure({ adapter: new Adapter() });
-  let rootComponent = null;
+  let rootComponent;
+
+  beforeEach(() => {
+    rootComponent = mount(<App />);
+  });
 
   it("renders without crashing", () => {
     const component = shallow(<App />);
@@ -23,19 +27,16 @@ describe('To test the App Component functionality.', () => {
   });
 
   it(`should mount and check if Provider component exists.`, () => {
-    rootComponent = mount(<App />);
     const isReduxProvider = rootComponent.find(Provider);
     expect(isReduxProvider).toHaveLength(1);
   });
 
   it(`should mount and check if PersistGate component exists.`, () => {
-    rootComponent = mount(<App />);
     const isPersistGate = rootComponent.find(PersistGate);
     expect(isPersistGate).toHaveLength(1);
   });
 
   it(`should mount and check if AppContainer component exists.`, () => {
-    rootComponent = mount(<App />);
     const isApplicationProvider = rootComponent.find(AppContainer);
     expect(isApplicationProvider).toHaveLength(1);
   });
