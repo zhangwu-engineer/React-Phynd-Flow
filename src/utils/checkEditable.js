70cf5f55a74f21c146cef58f23199fdfc8d38a40
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-24 04:11:03 -0500

    update helper - checkEditable

--- src/utils/checkEditable.js
+++ src/utils/checkEditable.js
@@ -1,16 +1,36 @@
+const uneditableParentNodes = [
+  'conditional-entity',
+  'switch-entity',
+  'regex-info',
+  'function-info',
+  'iteration-info',
+  'jsonproperty-info',
+  'aggregate-info',
+  'aggregate-iterator-info',
+  'jsonelement-info',
+  'jsonelement-operations',
+];
+
+const editableNodes = [
+  'Constant',
+  'Column',
+  'HL7',
+  'regex-info',
+  'function-info',
+  'iteration-info',
+  'jsonproperty-info',
+  'aggregate-info',
+  'aggregate-iterator-info',
+  'jsonelement-info'
+];
+
+const singleNodes = [
+  'Constant',
+  'Column',
+  'HL7'
+];
+
 export const checkCategoryEditable = (node) => {
-    const uneditableParentNodes = [
-      'conditional-entity',
-      'switch-entity',
-      'regex-info',
-      'function-info',
-      'iteration-info',
-      'jsonproperty-info',
-      'aggregate-info',
-      'aggregate-iterator-info',
-      'jsonelement-info',
-      'jsonelement-operations',
-    ];
     if ((node.edges && node.edges.length === 0) || node.parent) {
       if (uneditableParentNodes.indexOf(node.data.parentType) > -1)
         return false;
@@ -20,20 +40,9 @@ export const checkCategoryEditable = (node) => {
   }
   
   export const checkNodeEditable = node => (
-      node.data.parentType === 'Constant' ||
-      node.data.parentType === 'Column' ||
-      node.data.parentType === 'HL7' ||
-      node.data.parentType === 'regex-info' ||
-      node.data.parentType === 'function-info' ||
-      node.data.parentType === 'iteration-info' ||
-      node.data.parentType === 'jsonproperty-info' ||
-      node.data.parentType === 'aggregate-info' ||
-      node.data.parentType === 'aggregate-iterator-info' ||
-      node.data.parentType === 'jsonelement-info'
+      editableNodes.indexOf(node.data.parentType) > -1
     ) ? true : false;
 
   export const isSingleNode = nodeName => (
-      nodeName === 'Constant' ||
-      nodeName === 'Column' ||
-      nodeName === 'HL7'
+    singleNodes.indexOf(nodeName) > -1
     ) ? true : false;
