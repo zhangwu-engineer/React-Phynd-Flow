87fcc8134b6e13f37474b41ddbf0fb1a5b28eb35
Zhang Wu <zhangwu.engineer@gmail.com>
2020-02-24 04:06:22 -0500

    update helper checkCategoryEditable - uneditableParentNodes

--- src/utils/checkEditable.js
+++ src/utils/checkEditable.js
@@ -1,17 +1,18 @@
 export const checkCategoryEditable = (node) => {
+    const uneditableParentNodes = [
+      'conditional-entity',
+      'switch-entity',
+      'regex-info',
+      'function-info',
+      'iteration-info',
+      'jsonproperty-info',
+      'aggregate-info',
+      'aggregate-iterator-info',
+      'jsonelement-info',
+      'jsonelement-operations',
+    ];
     if ((node.edges && node.edges.length === 0) || node.parent) {
-      if (
-        node.data.parentType === 'conditional-entity' ||
-        node.data.parentType === 'switch-entity' ||
-        node.data.parentType === 'regex-info' ||
-        node.data.parentType === 'function-info' ||
-        node.data.parentType === 'iteration-info' ||
-        node.data.parentType === 'jsonproperty-info' ||
-        node.data.parentType === 'aggregate-info' ||
-        node.data.parentType === 'aggregate-iterator-info' ||
-        node.data.parentType === 'jsonelement-info' ||
-        node.data.parentType === 'jsonelement-operations'
-      )
+      if (uneditableParentNodes.indexOf(node.data.parentType) > -1)
         return false;
       return true;
     }
